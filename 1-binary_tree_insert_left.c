277e99092194bc19f6af54fc14d4e9961ff81796
LYDIA NAKIVUMBI <daniellawibz@gmail.com>
2023-12-08 16:15:23 +0100

    delete the entire tree

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -4,7 +4,7 @@
  *  binary_tree_insert_left - inserts a node as the left-child of another node.
  *  @parent: pointer to the node to insert the left-child.
  *  @value: value to store in the new node.
- *  
+ *
  *  Return: pointer to the new node or NULL.
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
