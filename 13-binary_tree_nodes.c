7e28f10e1df9a45cc2dcb3e835c7fb918410d11d
LYDIA NAKIVUMBI <daniellawibz@gmail.com>
2023-12-10 23:30:26 +0100

    Uncle

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -13,3 +13,33 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 
 	return (binary_tree_size(tree) - binary_tree_leaves(tree));
 }
+
+/**
+ * binary_tree_size - measures the size of a binary tree(no. of nodes).
+ * @tree: pointer to the root node.
+ * Return: 0(tree is NULL) else number.
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+}
+
+/**
+ * binary_tree_leaves - counts the leaves in a binary tree.
+ * @tree: pointer to the root node.
+ * Return: numbr of leaves or 0 (tree is NULL).
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+}
---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - finds the uncle of a node
+ * @node: pointer to the node to find the uncle
+ * Return:	NULL node is NULL
+ *			NULL node has no uncle
+ *			pointer to the uncle node
+ */
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (!node || !node->parent || !node->parent->parent)
+		return (NULL);
+	if (node->parent == node->parent->parent->left)
+		return (node->parent->parent->right);
+	else
+		return (node->parent->parent->left);
+}
